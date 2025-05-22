ad0b5368f24dfc6c9845119fdd73d771773644a6
joserqn <netojrqn@gmail.com>
2025-05-22 12:47:56 -0300

    commit 1

---
+++ src/modules/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from '../controllers/app.controller';
+import { AppService } from '../services/app.service';
+import { AuthModule } from 'src/modules/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'src/modules/prisma.module';
+
+@Module({
+  imports: [ 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }), 
+    PrismaModule, 
+    AuthModule.register(),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
